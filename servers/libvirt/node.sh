df29143b44512264266dd2754cb6db4f58bc6050
Michele Sciabarra <michele@sciabarra.com>
2018-02-10 23:13:07 +0100

    libvirt node creation

---
+++ servers/libvirt/node.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+
+NODE=${1:?node number}
+MEMORY=${2?memory}
+DISK=${3?disk size in giga}
+
+OSNAME=ubuntu16.04
+OSPATH=http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/
+ISO=ubuntu-16.04.3-server-amd64.iso
+PORT=$(expr 5920 + $NODE)
+IP=$(expr 10 + $NODE)
+
+if test "$NODE" = "0"
+then NAME="master"
+else NAME="node$NODE"
+fi
+
+test -e "$PWD/$NAME.img" || qemu-img create -f qcow2 "$PWD/$NAME.img" ${DISK}G
+
+virt-install \
+--name $NAME \
+--ram "$(expr $MEMORY \* 1024)" \
+--disk path=$PWD/$NAME.img \
+--vcpus 1 \
+--os-type linux \
+--os-variant $OSNAME \
+--network bridge=virbr2,mac=52:54:00:10:00:$IP \
+--graphics none \
+--console pty,target_type=serial \
+--location "$OSPATH" \
+--extra-args 'console=ttyS0' \
+--initrd-inject "$PWD/preseed.cfg" 
+
+#--graphics vnc,port=$PORT,listen=0.0.0.0 \
---
+++ servers/libvirt/setup.sh
@@ -0,0 +1,1 @@
+virsh net-create network.xml
